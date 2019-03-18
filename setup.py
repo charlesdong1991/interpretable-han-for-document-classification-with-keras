6484f2de349263860d8ad8ad6e056128121d2d6c
Kaiqi Dong <kaiqi@kth.se>
2019-03-18 22:56:04 +0100

    add setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='hierachical-attention-network-for-document-classification',
+    version='0.1.0',
+    packages=['han'],
+    url='',
+    author='Kaiqi Dong',
+    author_email='kaiqidong1991@gmail.com',
+    description='An Keras inplementation of Hierarchical Attention Networks for document'
+                ' classification.',
+    install_requires=[
+        'keras>=2.1.5'
+    ]
+)
